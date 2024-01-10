0cf80467c400d4f1a2db885a2184ac72b97c1f1c
dbasdanis <dbasdanis97@gmail.com>
2024-01-10 21:02:09 +0100

    create gradle project

---
+++ src/main/java/db/tc_msb/TcMsbApplication.java
@@ -0,0 +1,13 @@
+package db.tc_msb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcMsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcMsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/db/tc_msb/TcMsbApplicationTests.java
@@ -0,0 +1,13 @@
+package db.tc_msb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcMsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
